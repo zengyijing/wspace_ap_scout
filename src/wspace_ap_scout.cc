91c0177c60bd29dfe69592abe26a32adfb818681
zengyijing <zengyijing19900106@gmail.com>
2016-01-26 21:45:52 -0500

    fix SendLossRate when loss rate is invalid

--- src/wspace_ap_scout.cc
+++ src/wspace_ap_scout.cc
@@ -221,12 +221,14 @@ void WspaceAP::SendLossRate(int client_id) {
   char type = BS_STATS;
   static uint32 seq = 0;
   double throughput = 0;
-  double loss_rate, th;
+  double th = 0;
   LossMap *loss_map = client_context_tbl_[client_id]->scout_rate_maker()->GetLossMap(ScoutRateAdaptation::kBack);
-  for (int i = 0; i < mac80211abg_num_rates; i++) {
-    loss_rate = loss_map->GetLossRate(mac80211abg_rate[i]);
-    th = mac80211abg_rate[i] * (1 - loss_rate);
-    //printf("mac80211abg_rate[%d], loss_rate:%d\n", mac80211abg_rate[i], loss_rate);
+  for (int i = 0; i < mac80211abg_num_rates; ++i) {
+    double loss_rate = loss_map->GetLossRate(mac80211abg_rate[i]);
+    if (loss_rate != INVALID_LOSS_RATE)
+      th = mac80211abg_rate[i] * (1 - loss_rate);
+    else
+      continue;
     if(th > throughput)
       throughput = th;
   }
