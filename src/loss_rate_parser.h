103d0d6803555953c34cea19d49c33d315643e15
tanzhang0504 <tanzhang0504@gmail.com>
2016-02-18 23:34:56 -0600

    Add more hints to loss rate parser

--- src/loss_rate_parser.h
+++ src/loss_rate_parser.h
@@ -17,8 +17,10 @@ class LossRateParser {
   // <client_id, <bs_id, filename> >.
   void ParseLossRates(const vector<int> &rate_arr, unordered_map<int, unordered_map<int, string> > &filename_tbl);
   // @yijing: Pop queue for all the <client_id and bs_id>
+  // With lock.
   void UpdateLossRate();
   // @yijing: Get top of the queue.
+  // With lock.
   double GetLossRate(int client_id, int bs_id);
 
  private:
@@ -26,7 +28,6 @@ class LossRateParser {
   void Lock();
   void UnLock();
     
-  vector<int> rate_arr_; 
   unordered_map<int, unordered_map<int, queue<LossTable> > > loss_tbl_;  // <client_id, <bs_id, loss_table> >.
   pthread_mutex_t lock_;
 };
